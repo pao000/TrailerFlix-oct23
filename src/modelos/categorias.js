775ec2efa8d4914ff62091fcd69464b33e88aafe
Paola Gusmerotti <paogusme@hotmail.com>
2023-10-10 23:10:04 -0300

    my comment

--- src/modelos/categorias.js
+++ src/modelos/categorias.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../conexion/connection');
 
 const categorias = sequelize.define('categorias', {
-    id: {
+    categoriaID: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
